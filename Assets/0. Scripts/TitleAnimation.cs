34067f1ba9f78339d46756b43ec0cd93af2e22b6
JYJ <allen321@naver.com>
2024-03-26 03:25:08 +0900

    20240326.03:24
    Title Animation / TileMap

--- Assets/0. Scripts/TitleAnimation.cs
+++ Assets/0. Scripts/TitleAnimation.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using static UnityEngine.GraphicsBuffer;
 
 public class TitleAnimation : MonoBehaviour
 {
     [SerializeField] private Transform titleTrans;
     [SerializeField] private Transform startTrans;
+    [SerializeField] private Transform cameraTrans;
+
+    private Tween cameraTween;
     void Start()
     {
         Vector3 bigvector3 = new Vector3 (2f, 2f, 2f);
         Vector3 normalvector3 = new Vector3 (1f, 1f, 1f);
 
         SetAnimation();
-            
-            
-        
+        SetCamaraAnimation(10.0f);
+
+
+
     }
 
     void Update()
     {
-
-
+        //if (cameraTrans.transform.position.x < -10)
+        //{
+        //    SetCamaraAnimation();
+        //}
+   
     }
 
     void SetAnimation()
@@ -38,6 +46,26 @@ public class TitleAnimation : MonoBehaviour
     {
         titleTrans.DOShakePosition(5.0f, 5.0f).SetDelay(0.2f);
         startTrans.DOShakePosition(5.0f, 5.0f).SetDelay(0.2f);
+    }
+
+    void SetCamaraAnimation(float targetX)
+    {
+        cameraTween = cameraTrans.DOMoveX(targetX, 15.0f).SetDelay(0.5f).SetEase(Ease.Linear)
+             .OnKill(() => {
+                 // 货肺款 Tween 积己
+                 if (cameraTrans != null)
+                 {
+                     SetCamaraAnimation(targetX == 10f ? -10.001f : 10f);
+                 }
+             });
+    }
+
+    private void OnDestroy()
+    {
+        if(cameraTween != null)
+        {
+            cameraTween.Kill();
+        }
     }
 
 
